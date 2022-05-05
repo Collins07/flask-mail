7ff2cc7957ecebf41366547fe494f96ec2546f5e
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-06 01:56:21 +0300

    set config values

--- mail.py
+++ mail.py
@@ -3,12 +3,15 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']
-app.config['MAIL_PORT']
-app.config['MAIL_USE_TLS']
-app.config['MAIL_USE_SSL']
-app.config['MAIL_USERNAME']
-app.config['MAIL_PASSWORD']
-app.config['MAIL_DEFAULT_SENDER']
-app.config['MAIL_MAX_EMAILS']
-app.config['MAIL_SUPRESS_SENDER']
+app.config['DEBUG'] = True
+app.config['TESTING'] = False
+app.config['MAIL_SERVER'] =  'localhost'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = None
+app.config['MAIL_PASSWORD'] = None
+app.config['MAIL_DEFAULT_SENDER'] = None
+app.config['MAIL_MAX_EMAILS'] = None
+app.config['MAIL_SUPRESS_SENDER'] = False
